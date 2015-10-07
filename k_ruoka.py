5e11949fee0c3ea3d0cf130a0cb77af90c302cb9
Daniel Liljeqvist <daniel.liljeqvist@iki.fi>
2015-10-07 19:22:51 +0300

    Fixed K-ruoka scraper

--- k_ruoka.py
+++ k_ruoka.py
@@ -1,47 +1,26 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals, division
 from bs4 import BeautifulSoup
 import json
 import utils
 
+
 def scrape(url):
-	r = utils.getUrl(url)
-	bs = BeautifulSoup(r.content)
-	if not bs:
-		return
+    r = utils.getUrl(url)
+    bs = BeautifulSoup(r.content)
+    if not bs:
+        return
 
-	recipe={}
+    recipe = {}
 
-	title = bs.find("h2", {'class': "header-theme-color"})
-	name = title.get_text()
-	recipe["name"]=name
-	
-	ingredients = []
-	for row in bs.findAll("dl", {'itemprop': "ingredients"}):
-		amounts = []
-		names = []
-		for dts in row.findAll("dt"):
-			amount_unit = dts.get_text().strip()
-			amounts.append(amount_unit)
-		for dds in row.findAll("dd"):
-			name = dds.find("span", {'class':"ingredient-text"}).get_text().strip()
-			names.append(name)
+    j = bs.find('script', {'type': 'application/ld+json'})
+    data = json.loads(j.string)
+    print data
 
-		for idx, name in enumerate(names):
-			ingredient = {}
-			ingredient["amount"] = amounts[idx]
-			ingredient["name"] = name
-			ingredients.append(ingredient)
-
-	instructions = []
-	ul = bs.find("ul", {'itemprop':"recipeInstructions"})
-	for li in ul.findAll("li"):
-		instructions.append(li.findAll("span")[1].get_text())
-
-	recipe["instructions"]="".join(instructions)
-	recipe["ingredients"]=ingredients
-
-	return json.dumps(recipe)
-
-
+    recipe["name"] = data['description']
+    recipe["instructions"] = data['recipeInstructions']
+    recipe["ingredients"] = utils.split_amounts_and_ingredients(
+        data['recipeIngredient']
+        )
+    return json.dumps(recipe)
--- utils.py
+++ utils.py
@@ -1,34 +1,49 @@
 import requests
 
+
 def getUrl(url, nocache=False, params=None, headers=None, cookies=None):
-	"""Gets data, bs and headers for the given url, using the internal cache if necessary"""
+    """Gets data, bs and headers for the given url,
+    using the internal cache if necessary"""
 
-	browser = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11"
-	# Common session for all requests
-	s = requests.session()
-	s.verify = False
-	s.stream = True  # Don't fetch content unless asked
-	s.headers.update({'User-Agent': browser})
-	# Custom headers from requester
-	if headers:
-	    s.headers.update(headers)
-	# Custom cookies from requester
-	if cookies:
-	    s.cookies.update(cookies)
+    browser = """Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11
+    (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11"""
+    # Common session for all requests
+    s = requests.session()
+    s.verify = False
+    s.stream = True  # Don't fetch content unless asked
+    s.headers.update({'User-Agent': browser})
+    # Custom headers from requester
+    if headers:
+        s.headers.update(headers)
+    # Custom cookies from requester
+    if cookies:
+        s.cookies.update(cookies)
 
-	try:
-	    r = s.get(url, params=params)
-	except requests.exceptions.InvalidSchema:
-	    log.error("Invalid schema in URI: %s" % url)
-	    return None
-	except requests.exceptions.ConnectionError:
-	    log.error("Connection error when connecting to %s" % url)
-	    return None
+    try:
+        r = s.get(url, params=params)
+    except requests.exceptions.InvalidSchema:
+        print("Invalid schema in URI: %s" % url)
+        return None
+    except requests.exceptions.ConnectionError:
+        print("Connection error when connecting to %s" % url)
+        return None
 
-	size = int(r.headers.get('Content-Length', 0)) // 1024
-	#log.debug("Content-Length: %dkB" % size)
-	if size > 2048:
-	    log.warn("Content too large, will not fetch: %skB %s" % (size, url))
-	    return None
+    size = int(r.headers.get('Content-Length', 0)) // 1024
+    if size > 2048:
+        print("Content too large, will not fetch: %skB %s" % (size, url))
+        return None
 
-	return r
+    return r
+
+
+def split_amounts_and_ingredients(raw_data):
+    parsed = []
+    for item in raw_data:
+        if ')' in item:
+            amount, item = item.split(')')
+            parsed.append({'ingredient':item.strip(), 'amount':amount+(')').strip()})
+        else:
+            amount, item = item.split('  ')
+            parsed.append({'ingredient':item.strip(), 'amount':amount.strip()})
+
+    return parsed
